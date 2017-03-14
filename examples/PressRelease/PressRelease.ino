1278ed753fafa2b0d5e0b37d2fe46e5c3a2d4f2c
Gino Bollaert <gbollaert@gmail.com>
2017-03-15 00:03:36 +1100

    Update PressRelease.ino

--- examples/PressRelease/PressRelease.ino
+++ examples/PressRelease/PressRelease.ino
@@ -1,20 +1,27 @@
+/*
+ * Yet Another Button Library (for Arduino)
+ * https://github.com/yergin/Yabl
+ * Copyright 2017 Gino Bollaert
+ * MIT License (https://opensource.org/licenses/mit-license.php)
+ */
+
 #include <Yabl.h>
 
-#define BUTTON_PIN 6 // set this to the pin the button is connected to on your board
+#define BUTTON_PIN 6 // <- the board's pin the button is connectd to
 
 Button button;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  button.attach(BUTTON_PIN, INPUT_PULLUP);
+  button.attach(BUTTON_PIN, INPUT_PULLUP); // button also connected to GND
 }
 
 /*
- * pressed() and released() only return `true` after the first update() following
- * a button press/release.
+ * pressed() and released() will only return `true` immediately after a state change
+ * after update() is called
  */
 void loop() {
-  button.update();
+  button.update(); // check for button state changes
   if (button.pressed())
     digitalWrite(LED_BUILTIN, HIGH);
   else if (button.released())
