28c09eec2ca9a655c2b58f2ec19a6b0eac9e3a5d
Gino Bollaert <gbollaert@gmail.com>
2017-03-19 01:08:02 +1100

    add exit code 1

--- extras/verify_examples/verify_examples.sh
+++ extras/verify_examples/verify_examples.sh
@@ -6,18 +6,22 @@ else
 fi
 
 if [ ! -d $arduino_root ]; then
-  echo "Arduino not installed! Run install_arduino.sh"
+  echo "Arduino not installed! Run install_arduino.sh" >&2
   exit 1
 fi
 
 build_root=`pwd`/build
-
+exit_code=0
 for sketch in `find ../../examples -name '*.ino'`
 do
-  echo $sketch
+  echo "Building "$sketch
   sketch_build_dir=$(basename $sketch)
   mkdir -p $build_root/$sketch_build_dir
 
   cd $arduino_root
   ./arduino-builder -hardware ./hardware -tools ./hardware/tools/avr -tools ./tools-builder -libraries ./libraries -libraries $build_root/../libraries -fqbn arduino:avr:uno -build-path $build_root/$sketch_build_dir $build_root/../$sketch
+
+  [ $? == 0 ] && rm -rf $build_root/$sketch_build_dir || exit_code=1
 done
+
+[ $exit_code == 0 ] && rm -rf $build_root || exit 1
