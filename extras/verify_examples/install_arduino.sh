1c8acc34b02052fe843ef4ef168ec608a5077572
Gino Bollaert <gbollaert@gmail.com>
2017-03-19 01:21:02 +1100

    only install arduino if not present

--- extras/verify_examples/install_arduino.sh
+++ extras/verify_examples/install_arduino.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-[[ -d Arduino ]] || git clone https://github.com/arduino/Arduino.git
+[[ ! -d Arduino ]] || exit 0
+git clone https://github.com/arduino/Arduino.git
 cd Arduino/build
-#echo 1.8.2 | ant clean dist
+echo 1.8.2 | ant clean dist
 if [ "$(uname)" == "Darwin" ]; then
+  arduino_root=`pwd`/macosx/work/Arduino.app/Contents/Java
   arduino_binary=`pwd`/macosx/work/Arduino.app/Contents/MacOS/Arduino
 else
+  arduino_root=`pwd`/linux/work
   arduino_binary=`pwd`/linux/work/arduino
 fi
 # this doesn't work for some reason
 # $arduino_binary --install-library "Bounce2"
-cd ../../libraries
+cd $arduino_root/libraries
 git clone https://github.com/thomasfredericks/Bounce2.git
